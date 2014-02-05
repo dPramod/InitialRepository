3e484abda9814b313f776ee5a3cdc7c2b5e4a5f8
Pramod <dPramod>
2014-02-05 15:48:20 +0530

    Initial Commit

---
+++ Training.Git.Repository/src/Training/Git/Repository/InitialCommit/HelloWorld.java
@@ -0,0 +1,16 @@
+package Training.Git.Repository.InitialCommit;
+/**
+ * 
+ * @author Pramod Divakarla
+ * This is the first file to test on how git works with eclipse
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
